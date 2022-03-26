db52d5c5466e36ba7b05a2c756737686b445d96d
HEVERHD <hever11.hdgd@gmail.com>
2022-03-26 15:08:55 -0500

    SliderBar

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -1,19 +1,209 @@
-import style from './Sidebar.module.scss'
-
+import style from './Sidebar.module.scss';
 import { useNavigate } from 'react-router-dom';
-import { Button } from '@mui/material'
+import { Button } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import PersonIcon from '@mui/icons-material/Person';
+import CategoryIcon from '@mui/icons-material/Category';
+import PaidIcon from '@mui/icons-material/Paid';
 
-const Sidebar = ()=>{
-      const navigate = useNavigate();
+const drawerWidth = 240;
 
-      return (
-            <div className={style.container}>
-                  <Button variant="outlined" className='btn_mui' onClick={()=> navigate('/home')}>Home</Button>
-                  <Button variant="outlined" className='btn_mui' onClick={()=>navigate('/users')}>Users</Button>
-                  <Button variant="outlined" className='btn_mui' onClick={()=>navigate('/categories')}>Categories</Button>
-                  <Button variant="outlined" className='btn_mui' onClick={()=>navigate('/transactions')}>Transactions</Button>
+const openedMixin = (theme) => ({
+	width: drawerWidth,
+	transition: theme.transitions.create('width', {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.enteringScreen,
+	}),
+	overflowX: 'hidden',
+});
 
-            </div>
-      )
-}
+const closedMixin = (theme) => ({
+	transition: theme.transitions.create('width', {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.leavingScreen,
+	}),
+	overflowX: 'hidden',
+	width: `calc(${theme.spacing(7)} + 1px)`,
+	[theme.breakpoints.up('sm')]: {
+		width: `calc(${theme.spacing(8)} + 1px)`,
+	},
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'flex-end',
+	padding: theme.spacing(0, 1),
+	// necessary for content to be below app bar
+	...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+	shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+	zIndex: theme.zIndex.drawer + 1,
+	transition: theme.transitions.create(['width', 'margin'], {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.leavingScreen,
+	}),
+	...(open && {
+		marginLeft: drawerWidth,
+		width: `calc(100% - ${drawerWidth}px)`,
+		transition: theme.transitions.create(['width', 'margin'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	}),
+}));
+
+const Drawer = styled(MuiDrawer, {
+	shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+	width: drawerWidth,
+	flexShrink: 0,
+	whiteSpace: 'nowrap',
+	boxSizing: 'border-box',
+	...(open && {
+		...openedMixin(theme),
+		'& .MuiDrawer-paper': openedMixin(theme),
+	}),
+	...(!open && {
+		...closedMixin(theme),
+		'& .MuiDrawer-paper': closedMixin(theme),
+	}),
+}));
+
+const Sidebar = () => {
+	const theme = useTheme();
+	const [open, setOpen] = React.useState(false);
+
+	const handleDrawerOpen = () => {
+		setOpen(true);
+	};
+
+	const handleDrawerClose = () => {
+		setOpen(false);
+	};
+
+	const navigate = useNavigate();
+
+	return (
+		<>
+			<Box sx={{ display: 'flex' }}>
+				<CssBaseline />
+				<AppBar color ='secondary'
+                         position='fixed' open={open}>
+					<Toolbar>
+						<IconButton
+							color='inherit'
+							aria-label='open drawer'
+							onClick={handleDrawerOpen}
+							edge='start'
+							sx={{
+								marginRight: 5,
+								...(open && { display: 'none' }),
+							}}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography variant='h6' noWrap component='div'>
+							Panel Administrativo Everyone
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				<Drawer variant='permanent' open={open}>
+					<DrawerHeader>
+						<IconButton onClick={handleDrawerClose}>
+							{theme.direction === 'rtl' ? (
+								<ChevronRightIcon />
+							) : (
+								<ChevronLeftIcon />
+							)}
+						</IconButton>
+					</DrawerHeader>
+					<Divider />
+					<List>
+						{['Lista Productos ', 'Usuarios',].map((text, index) => (
+							<ListItemButton
+								key={text}
+								sx={{
+									minHeight: 48,
+									justifyContent: open ? 'initial' : 'center',
+									px: 2.5,
+								}}
+							>
+								<ListItemIcon
+									sx={{
+										minWidth: 0,
+										mr: open ? 3 : 'auto',
+										justifyContent: 'center',
+									}}
+								>
+									{index % 2 === 0 ? (
+										<InboxIcon onClick={() => navigate('/home')} />
+									) : (
+										<PersonIcon onClick={() => navigate('/users')} />
+									)}
+                                                      
+								</ListItemIcon>
+                                                
+								<ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                                                
+							</ListItemButton>
+						))}
+					</List>
+					<List>
+						{['Categorias', 'Transaccion'].map((text, index) => (
+							<ListItemButton
+								key={text}
+								sx={{
+									minHeight: 48,
+									justifyContent: open ? 'initial' : 'center',
+									px: 2.5,
+								}}
+							>
+								<ListItemIcon
+									sx={{
+										minWidth: 0,
+										mr: open ? 3 : 'auto',
+										justifyContent: 'center',
+									}}
+								>
+									{index % 2 === 0 ? (
+										<CategoryIcon onClick={() => navigate('/categories')} />
+									) : (
+										<PaidIcon onClick={()=>navigate('/transactions')} />
+									)}
+								</ListItemIcon>
+								<ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+							</ListItemButton>
+						))}
+					</List>
+					<Divider />
+				</Drawer>
+				<Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+					<DrawerHeader />
+					<></>
+				</Box>
+			</Box>
+		</>
+	);
+};
 export default Sidebar;
--- src/index.js
+++ src/index.js
@@ -8,20 +8,28 @@ import { ToastContainer } from 'react-toastify';
 import {Provider} from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import {store, persistor} from './redux/store';
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@material-ui/core";
+import Themes from "../src/themes";
+import { LayoutProvider } from "./context/LayoutContext";
+import { UserProvider } from "./context/UserContext";
 
 ReactDOM.render(
-  <>
+  <LayoutProvider>
+  <UserProvider>
     <BrowserRouter>
-
+<ThemeProvider theme={Themes.default}>
       <Provider store={store}>
+      <CssBaseline />
         <PersistGate persistor={persistor}>
           <App />
         </PersistGate>
       </Provider>
-
+      </ThemeProvider>
     </BrowserRouter>
     <ToastContainer/>
-  </>,
+    </UserProvider>
+  </LayoutProvider>,
   document.getElementById('root')
 );
 
