e7f25c13b876eec1b91a1eb4974861204410cc26
BrunoAmerio <88524640+BrunoAmerio@users.noreply.github.com>
2022-03-29 12:00:04 -0300

    Add some images

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,12 @@
 import { initializeApp } from "firebase/app";
-const firebaseConfing = () => {
-      const firebaseConfig = {
-            apiKey: "AIzaSyCLPv_0OD28irbXx8efWvki-_JeESAmdss",
-            authDomain: "everyones-app.firebaseapp.com",
-            projectId: "everyones-app",
-            storageBucket: "everyones-app.appspot.com",
-      };
 
-      const app = initializeApp(firebaseConfig);
-      return app;
-}     
+const firebaseConfig = {
+      apiKey: "AIzaSyCLPv_0OD28irbXx8efWvki-_JeESAmdss",
+      authDomain: "everyones-app.firebaseapp.com",
+      projectId: "everyones-app",
+      storageBucket: "everyones-app.appspot.com",
+};
 
-export default firebaseConfing
+const app = initializeApp(firebaseConfig);
+
+export default app;
